f63992be6e56e0e5a49a84b46a07a586bdaffeed
strkto <67833385+strkto@users.noreply.github.com>
2020-08-06 12:20:12 +0300

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Константин Стародубцев on 06.08.2020.
+//  Copyright © 2020 Constantin Starodubtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
