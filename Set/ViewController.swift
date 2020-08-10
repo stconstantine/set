c27c545a4689c4d1197dbf96ad01f4deed4a2814
strkto <67833385+strkto@users.noreply.github.com>
2020-08-10 17:06:01 +0300

    builded the simple variant of UI

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -12,19 +12,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    private func printCard(_ theCard: Card) {
-        print("Number: \(theCard.number), shape: \(theCard.thing.shape), shading: \(theCard.thing.shading), color: \(theCard.thing.color)")
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCards(_ sender: UIButton) {
     }
     
-    var deck = Deck()
-    
-    let game = SetGame()
-    
-    @IBAction func touchDraw(_ sender: UIButton) {
-
-        print("testCardsNotSet is set (must be false): \(game.setMade(with: SetGame.testCardsNotSet))")
-        print("testCardsSet is set (must be true): \(game.setMade(with: SetGame.testCardsSet))")
+    @IBAction func deal(_ sender: UIButton) {
     }
+    
+    @IBOutlet weak var score: UILabel!
 }
+
