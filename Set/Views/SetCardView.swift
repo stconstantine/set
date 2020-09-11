36bf82d8d75e90785691840d9ddcda81d7d94090
strkto <67833385+strkto@users.noreply.github.com>
2020-09-11 20:52:16 +0300

    Implemented drawing of card with items, rewrote class

--- Set/Views/SetCardView.swift
+++ Set/Views/SetCardView.swift
@@ -7,113 +7,155 @@
 //
 
 import UIKit
+//import CoreGraphics
 
+@IBDesignable
 class SetCardView: UIView {
-
-    override func draw(_ rect: CGRect) {
-
-        let card = Card(shape: .wave,
-                        shading: .clear,
-                        color: .one,
-                        number: .one)
-        
-        decorate(the: thing(for: card, in: bounds),
-                 colorizeWith: card.color.drawColor,
-                 shadeWith: card.shading)
-
+    //MARK: Constants
+    private var itemBorderWidth: CGFloat {
+        return max(max(bounds.width,bounds.height)/150,1)
     }
+    private var cardCornerRadius: CGFloat {
+        return itemBorderWidth * 1.5
+    }
+    @IBInspectable private var cardBackgroundColor: UIColor = .systemBackground
     
-    func thing(for card: Card, in frame: CGRect) -> UIBezierPath {
-        let undecoratedThing: UIBezierPath
-        switch card.shape {
-        case .diamond: undecoratedThing = diamond(in: frame)
-        case .wave: undecoratedThing = squiggle(in: frame)
-        case .oval: undecoratedThing = oval(in: frame)
-        }
-        return undecoratedThing
-    }
-    
-    func decorate(the thing: UIBezierPath, colorizeWith color: UIColor, shadeWith shading: Card.Shadings) {
-        thing.lineCapStyle = .round
-        thing.lineJoinStyle = .round
-        thing.lineWidth = max(thing.bounds.width,thing.bounds.height)/30
-        thing.addClip()
-        color.setStroke()
-        thing.stroke()
-        var fillColor: UIColor = .white
-        switch shading {
-        case .solid: fillColor = color
-        case .stripped: return // сделать заливку полосками
-        case .clear: return
-        }
-        fillColor.setFill()
-        thing.fill()
-    }
-    func diamond(in frame: CGRect) -> UIBezierPath {
-        let path = UIBezierPath()
-        
-        path.move(to: CGPoint(x: frame.midX,
-                              y: frame.minY))
-        path.addLine(to: CGPoint(x: frame.maxX,
-                                 y: frame.midY))
-        path.addLine(to: CGPoint(x: frame.midX,
-                                 y: frame.maxY))
-        path.addLine(to: CGPoint(x: frame.minX,
-                                 y: frame.midY))
-        path.close()
-        return path
-    }
-    func squiggle(in frame: CGRect) -> UIBezierPath {
-        let path = UIBezierPath()
-        let curvness: CGFloat = 0.45
-        let lines = [frame.midY-frame.maxY*curvness/2, frame.midY+frame.maxY*curvness/2]
-        let xGap: CGFloat = 0.2
-        let corr: CGFloat = 0.825
-    
-        let basePoints = [CGPoint(x: frame.maxX*xGap, y: lines[0]),
-                          CGPoint(x: frame.midX, y: lines[0]),
-                          CGPoint(x: frame.maxX*(1-xGap), y: lines[0]),
-                          CGPoint(x: frame.maxX*xGap, y: lines[1]),
-                          CGPoint(x: frame.midX, y: lines[1]),
-                          CGPoint(x: frame.maxX*(1-xGap), y: lines[1])
-        ]
-        
-        let controlPoints = [CGPoint(x: (basePoints[0].x+basePoints[1].x)/2, y: lines[0]-frame.maxY*curvness),
-                             CGPoint(x: (basePoints[1].x+basePoints[2].x)/2, y: lines[0]+frame.maxY*curvness),
-                             CGPoint(x: (basePoints[3].x+basePoints[4].x)/2, y: lines[1]-frame.maxY*curvness),
-                             CGPoint(x: (basePoints[4].x+basePoints[5].x)/2, y: lines[1]+frame.maxY*curvness),
-                             CGPoint(x: frame.maxX*(corr+xGap), y: frame.minY-frame.maxY*(curvness-0.1)),
-                             CGPoint(x: frame.minX-frame.maxX*(xGap-1+corr), y: frame.maxY*(curvness+0.9)),
-                             
-        ]
-        
-        path.move(to: basePoints[0])
-        path.addQuadCurve(to: basePoints[1], controlPoint: controlPoints[0])
-        path.addQuadCurve(to: basePoints[2], controlPoint: controlPoints[1])
-        
-        path.move(to: basePoints[3])
-        path.addQuadCurve(to: basePoints[4], controlPoint: controlPoints[2])
-        path.addQuadCurve(to: basePoints[5], controlPoint: controlPoints[3])
-        
-        path.move(to: basePoints[5])
-        path.addQuadCurve(to: basePoints[2], controlPoint: controlPoints[4])
-        
-        path.move(to: basePoints[0])
-        path.addQuadCurve(to: basePoints[3], controlPoint: controlPoints[5])
-        
-        return path
-    }
-    func oval(in frame: CGRect) -> UIBezierPath {
-        return UIBezierPath(ovalIn: frame)
-    }
-}
-
-extension Card.Colors {
-    var drawColor: UIColor {
-        switch self {
+    var itemColor: ItemColor = .three
+    @IBInspectable private var drawColor: UIColor {
+        switch itemColor {
         case .one: return .systemRed
         case .two: return .systemYellow
         case .three: return .systemBlue
         }
     }
+    
+    var itemShading: ItemShading = .clear
+    var thingShape: ItemShape = .squiggle
+    var thingCount: Int = 3
+    var isSelected: Bool = false
+    var isMatched: Bool = false
+    
+    override func draw(_ rect: CGRect) {
+        
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cardCornerRadius)
+        cardBackgroundColor.setFill()
+        roundedRect.fill()
+        drawCard()
+    }
+    
+    private func drawCard() {
+        //функция рисует нужное количество элементов things на карточке
+        
+        //создаём массив фреймов для элементов и массив начальных точек для расположения этих элементов
+        var thingPlace = [CGRect]()
+        var thingPlaceOrigin = [CGPoint]()
+        
+        //Создаём размер фрейма
+        let thingPlaceSize = CGSize(width: layer.bounds.maxX*0.8,
+                                    height: layer.bounds.maxY*0.2)
+        
+        //Заполняем массив начальных точек в зависимости от необходимого количества элементов к отрисовке
+        switch thingCount {
+        case 2:
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)*0.3))
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)*0.7))
+        case 3:
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)*0.5))
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)*0.125))
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)*0.875))
+        default:
+            thingPlaceOrigin.append(CGPoint(x: (bounds.width-thingPlaceSize.width)/2,
+                                         y: (bounds.height-thingPlaceSize.height)/2))
+        }
+        
+        //заполняем массив фреймов — создаём прямоугольник для каждой начальной точки
+        thingPlace = thingPlaceOrigin.map {CGRect(origin: $0, size: thingPlaceSize)}
+        //рисуем каждый прямоугольник и фигурку в нём
+        thingPlace.forEach {drawThing(in: $0)}
+    }
+    
+    private func drawThing(in frame: CGRect) {
+        var thing: UIBezierPath
+        switch thingShape {
+        case .squiggle: thing = squiggle(in: frame)
+        case .oval:  thing = oval(in: frame)
+        case .diamond: thing = diamond(in: frame)
+        }
+        if itemShading == .solid {
+            drawColor.setFill()
+        } else {cardBackgroundColor.setFill()}
+        
+        if itemShading == .stripped {
+            drawStripes(in: frame)
+        }
+        
+        thing.fill()
+        thing.lineWidth = itemBorderWidth
+        drawColor.setStroke()
+        thing.stroke()
+    }
+    
+    func drawStripes(in frame: CGRect) {
+        
+    }
+
+    //MARK: thing pathes
+    private func diamond(in frame: CGRect) -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: frame.midX, y: frame.minY))
+        path.addLine(to: CGPoint(x: frame.maxX,y: frame.midY))
+        path.addLine(to: CGPoint(x: frame.midX,y: frame.maxY))
+        path.addLine(to: CGPoint(x: frame.minX,y: frame.midY))
+        path.close()
+        return path
+    }
+    private func squiggle(in frame: CGRect) -> UIBezierPath {
+        let upperSquiggle = UIBezierPath()
+        let sqdx = frame.width * 0.1
+        let sqdy = frame.height * 0.2
+        upperSquiggle.move(to: CGPoint(x: frame.minX, y: frame.midY))
+        upperSquiggle.addCurve(to:
+            CGPoint(x: frame.minX + frame.width * 1/2,
+                    y: frame.minY + frame.height / 8),
+                               controlPoint1: CGPoint(x: frame.minX,
+                                                      y: frame.minY),
+                               controlPoint2: CGPoint(x: frame.minX + frame.width * 1/2 - sqdx,
+                                                      y: frame.minY + frame.height / 8 - sqdy))
+        upperSquiggle.addCurve(to:
+            CGPoint(x: frame.minX + frame.width * 4/5,
+                    y: frame.minY + frame.height / 8),
+                               controlPoint1: CGPoint(x: frame.minX + frame.width * 1/2 + sqdx,
+                                                      y: frame.minY + frame.height / 8 + sqdy),
+                               controlPoint2: CGPoint(x: frame.minX + frame.width * 4/5 - sqdx,
+                                                      y: frame.minY + frame.height / 8 + sqdy))
+        
+        upperSquiggle.addCurve(to:
+            CGPoint(x: frame.minX + frame.width,
+                    y: frame.minY + frame.height / 2),
+                               controlPoint1: CGPoint(x: frame.minX + frame.width * 4/5 + sqdx,
+                                                      y: frame.minY + frame.height / 8 - sqdy ),
+                               controlPoint2: CGPoint(x: frame.minX + frame.width,
+                                                      y: frame.minY))
+        
+        let lowerSquiggle = UIBezierPath(cgPath: upperSquiggle.cgPath)
+        lowerSquiggle.apply(CGAffineTransform.identity.rotated(by: CGFloat.pi))
+        lowerSquiggle.apply(CGAffineTransform.identity.translatedBy(
+            x: bounds.width,
+            y: bounds.height))
+        upperSquiggle.move(to: CGPoint(x: frame.minX, y: frame.midY))
+        upperSquiggle.append(lowerSquiggle)
+        return upperSquiggle
+    }
+    private func oval(in frame: CGRect) -> UIBezierPath {
+        return UIBezierPath(ovalIn: frame)
+    }
+    
+    enum ItemShape: CaseIterable {case diamond, squiggle, oval}
+    enum ItemShading: CaseIterable {case clear, stripped, solid}
+    enum ItemColor: CaseIterable {case one, two, three}
 }
